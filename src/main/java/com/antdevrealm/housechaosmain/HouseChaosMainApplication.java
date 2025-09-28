e25ba7038f87e6fd233c6d86d3bf3bc6f257c983
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-09-28 11:34:25 +0200

    Intial commit

---
+++ src/main/java/com/antdevrealm/housechaosmain/HouseChaosMainApplication.java
@@ -0,0 +1,13 @@
+package com.antdevrealm.housechaosmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseChaosMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseChaosMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/antdevrealm/housechaosmain/HouseChaosMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antdevrealm.housechaosmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseChaosMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
