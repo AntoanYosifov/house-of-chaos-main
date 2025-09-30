0bee27c39fb38d765d6ed236743e6dc3b55cc7dd
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-09-30 18:54:54 +0200

    Successful logic on mock protected endpoint. Valid token - allowed. Invalid token - denied

--- src/main/java/com/antdevrealm/housechaosmain/features/user/web/AuthController.java
+++ src/main/java/com/antdevrealm/housechaosmain/features/user/web/AuthController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -51,6 +48,12 @@ public class AuthController {
 
         TokenResponse tokenResponse = new TokenResponse(token, "Bearer", jwtService.ttlSeconds());
         return ResponseEntity.ok(tokenResponse);
+    }
+
+    // Test authenticated end point
+    @GetMapping("/protected")
+    public ResponseEntity<String> getProtected() {
+        return ResponseEntity.ok("You got here");
     }
 
 }
