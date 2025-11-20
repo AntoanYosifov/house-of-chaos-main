b909be344ec3d20fc6de4e31f1fca5903862edce
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-11-20 14:24:42 +0100

    chore(address) - reformat AddressRequestDTO for better readability

--- src/main/java/com/antdevrealm/housechaosmain/address/dto/AddressRequestDTO.java
+++ src/main/java/com/antdevrealm/housechaosmain/address/dto/AddressRequestDTO.java
@@ -7,11 +7,14 @@ import jakarta.validation.constraints.NotNull;
 public record AddressRequestDTO(
         @NotBlank(message = "Country is required")
         String country,
+
         @NotBlank(message = "City is required")
         String city,
+
         @NotNull(message = "Zip is required")
         @Min(value = 1, message = "Zip must be positive")
         Integer zip,
+
         @NotBlank(message = "Street is required")
         String street) {
 }
