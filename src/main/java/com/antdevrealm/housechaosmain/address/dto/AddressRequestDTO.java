5901f00f6373c7d93987ded2a89979c7bebbdb77
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-11-20 14:08:22 +0100

    feat(address) - add validation annotations on AddressRequestDTO

--- src/main/java/com/antdevrealm/housechaosmain/address/dto/AddressRequestDTO.java
+++ src/main/java/com/antdevrealm/housechaosmain/address/dto/AddressRequestDTO.java
@@ -1,9 +1,18 @@
 package com.antdevrealm.housechaosmain.address.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record AddressRequestDTO(
+        @NotBlank(message = "Country is required")
         String country,
+        @NotBlank(message = "City is required")
         String city,
-        int zip,
+        @NotNull(message = "Zip is required")
+        @Min(value = 1, message = "Zip must be positive")
+        Integer zip,
+        @NotBlank(message = "Street is required")
         String street
 ) {
 }
