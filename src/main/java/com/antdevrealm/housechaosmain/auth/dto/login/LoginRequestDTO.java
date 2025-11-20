3ca436e13bdcf90a2d5e3e951c040bc58ea3bece
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-11-20 14:18:53 +0100

    feat(login) - add messages in the validation annotations on LoginRequestDTO

--- src/main/java/com/antdevrealm/housechaosmain/auth/dto/login/LoginRequestDTO.java
+++ src/main/java/com/antdevrealm/housechaosmain/auth/dto/login/LoginRequestDTO.java
@@ -3,6 +3,11 @@ package com.antdevrealm.housechaosmain.auth.dto.login;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record LoginRequestDTO(@NotBlank @Email String email,
-                              @NotBlank String password) {
+public record LoginRequestDTO(
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email is not valid")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        String password) {
 }
