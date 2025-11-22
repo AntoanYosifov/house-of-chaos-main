59b089afa4dd13b9a13e37534641b46a157a50c3
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-11-22 19:05:08 +0100

    feat(category) - add CategoryController

---
+++ src/main/java/com/antdevrealm/housechaosmain/category/web/CategoryController.java
@@ -0,0 +1,28 @@
+package com.antdevrealm.housechaosmain.category.web;
+
+import com.antdevrealm.housechaosmain.category.dto.CategoryResponseDTO;
+import com.antdevrealm.housechaosmain.category.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponseDTO>> getAll() {
+        List<CategoryResponseDTO> allCategories = categoryService.getAll();
+        return ResponseEntity.ok(allCategories);
+    }
+}
