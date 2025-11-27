f91f30d07907d26f6d03d0adf8c9fcefcc3a7859
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-11-27 22:15:20 +0100

    chore(security) - add  "/api/v1/categories/**" to permit all endpoints

--- src/main/java/com/antdevrealm/housechaosmain/config/SecurityConfig.java
+++ src/main/java/com/antdevrealm/housechaosmain/config/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig {
                                 .requestMatchers(
                                         "/images/**",
                                         "/api/v1/products/**",
+                                        "/api/v1/categories/**",
                                         "/api/v1/users/register",
                                         "/api/v1/auth/login",
                                         "/api/v1/auth/logout",
