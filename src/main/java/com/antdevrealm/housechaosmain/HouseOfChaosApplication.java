b5b692b0fd7ff04b4ae66d9602d551098b29cc60
AntoanYosifov <tapaktapxaomi@gmail.com>
2026-02-21 08:24:34 +0100

    feat(pagination) - add @EnableSpringDataWebSupport globally for stable page json serialization

--- src/main/java/com/antdevrealm/housechaosmain/HouseOfChaosApplication.java
+++ src/main/java/com/antdevrealm/housechaosmain/HouseOfChaosApplication.java
@@ -4,16 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @EnableCaching
 @EnableScheduling
-@SpringBootApplication
 @EnableFeignClients
-public class HouseOfChaosApplication {
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 
+@SpringBootApplication
+public class HouseOfChaosApplication {
     public static void main(String[] args) {
         SpringApplication.run(HouseOfChaosApplication.class, args);
     }
-
 }
+
